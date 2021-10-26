847339f39b06186d09c5aba77ec6d575ed011ba0
ori.blanka <ori.blanka>
2021-10-26 14:23:54 +0300

    Adding pictureBox and label

--- A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsApp/FormMain.Designer.cs
+++ A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsApp/FormMain.Designer.cs
@@ -34,16 +34,18 @@
             this.m_MainTab = new System.Windows.Forms.TabPage();
             this.m_FetchEventsTab = new System.Windows.Forms.TabPage();
             this.m_MutualLikedPagesTab = new System.Windows.Forms.TabPage();
+            this.m_UserProfilePicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
-            this.m_MainTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_UserProfilePicture)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(26, 27);
+            this.buttonLogin.Location = new System.Drawing.Point(17, 16);
             this.buttonLogin.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(477, 55);
+            this.buttonLogin.Size = new System.Drawing.Size(193, 55);
             this.buttonLogin.TabIndex = 36;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -51,10 +53,10 @@
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(26, 96);
+            this.buttonLogout.Location = new System.Drawing.Point(2031, 16);
             this.buttonLogout.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(477, 55);
+            this.buttonLogout.Size = new System.Drawing.Size(193, 55);
             this.buttonLogout.TabIndex = 52;
             this.buttonLogout.Text = "Logout";
             this.buttonLogout.UseVisualStyleBackColor = true;
@@ -65,56 +67,77 @@
             this.tabControl1.Controls.Add(this.m_MainTab);
             this.tabControl1.Controls.Add(this.m_FetchEventsTab);
             this.tabControl1.Controls.Add(this.m_MutualLikedPagesTab);
-            this.tabControl1.Location = new System.Drawing.Point(2, 12);
+            this.tabControl1.Font = new System.Drawing.Font("Miriam", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(17, 238);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(2231, 1397);
+            this.tabControl1.Size = new System.Drawing.Size(2212, 1171);
             this.tabControl1.TabIndex = 53;
             // 
             // m_MainTab
             // 
-            this.m_MainTab.Controls.Add(this.buttonLogout);
-            this.m_MainTab.Controls.Add(this.buttonLogin);
-            this.m_MainTab.Location = new System.Drawing.Point(10, 48);
+            this.m_MainTab.Location = new System.Drawing.Point(10, 51);
             this.m_MainTab.Name = "m_MainTab";
             this.m_MainTab.Padding = new System.Windows.Forms.Padding(3);
-            this.m_MainTab.Size = new System.Drawing.Size(2211, 1339);
+            this.m_MainTab.Size = new System.Drawing.Size(2192, 1110);
             this.m_MainTab.TabIndex = 0;
             this.m_MainTab.Text = "Main";
             this.m_MainTab.UseVisualStyleBackColor = true;
             // 
             // m_FetchEventsTab
             // 
-            this.m_FetchEventsTab.Location = new System.Drawing.Point(10, 48);
+            this.m_FetchEventsTab.Location = new System.Drawing.Point(10, 51);
             this.m_FetchEventsTab.Name = "m_FetchEventsTab";
             this.m_FetchEventsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.m_FetchEventsTab.Size = new System.Drawing.Size(2211, 1339);
+            this.m_FetchEventsTab.Size = new System.Drawing.Size(2192, 1110);
             this.m_FetchEventsTab.TabIndex = 1;
             this.m_FetchEventsTab.Text = "My Events";
             this.m_FetchEventsTab.UseVisualStyleBackColor = true;
             // 
             // m_MutualLikedPagesTab
             // 
-            this.m_MutualLikedPagesTab.Location = new System.Drawing.Point(10, 48);
+            this.m_MutualLikedPagesTab.Location = new System.Drawing.Point(10, 51);
             this.m_MutualLikedPagesTab.Name = "m_MutualLikedPagesTab";
-            this.m_MutualLikedPagesTab.Size = new System.Drawing.Size(2211, 1339);
+            this.m_MutualLikedPagesTab.Size = new System.Drawing.Size(2192, 1110);
             this.m_MutualLikedPagesTab.TabIndex = 2;
             this.m_MutualLikedPagesTab.Text = "Mutual Liked Pages";
             this.m_MutualLikedPagesTab.UseVisualStyleBackColor = true;
+            // 
+            // m_UserProfilePicture
+            // 
+            this.m_UserProfilePicture.Location = new System.Drawing.Point(936, 16);
+            this.m_UserProfilePicture.Name = "m_UserProfilePicture";
+            this.m_UserProfilePicture.Size = new System.Drawing.Size(210, 216);
+            this.m_UserProfilePicture.TabIndex = 54;
+            this.m_UserProfilePicture.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1175, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "label1";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2241, 1421);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m_UserProfilePicture);
+            this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buttonLogin);
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
-            this.m_MainTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.m_UserProfilePicture)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,6 +149,8 @@
         private System.Windows.Forms.TabPage m_MainTab;
         private System.Windows.Forms.TabPage m_FetchEventsTab;
         private System.Windows.Forms.TabPage m_MutualLikedPagesTab;
+        private System.Windows.Forms.PictureBox m_UserProfilePicture;
+        private System.Windows.Forms.Label label1;
     }
 }
 
