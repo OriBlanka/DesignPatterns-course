9f896d08ddac20249a6a6773918caa9ee5d18ea4
ori.blanka <ori.blanka>
2021-12-25 23:57:27 +0200

    start to impliment filterCelebrtyNamr

--- A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsLogic/CelebrityBirthdayAPI.cs
+++ A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsLogic/CelebrityBirthdayAPI.cs
@@ -11,49 +11,50 @@ namespace FacebookWinFormsLogic
     // Todo check if api works
     internal class CelebrityBirthdayAPI
     {
-
+        private const string k_ApiKey = "371d19e25cmsh2efefec9d8503bcp1c6603jsn55c3cb22f273";
         /*private static readonly HttpClient sr_HttpClient = new HttpClient();
         HttpRequestMessage request = new HttpRequestMessage*/
         public async Task<XDocument> ApiTest()
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://celebrity-bucks.p.rapidapi.com/birthdays/XML"),
                 Headers =
-    {
-        { "x-rapidapi-host", "celebrity-bucks.p.rapidapi.com" },
-        { "x-rapidapi-key", "371d19e25cmsh2efefec9d8503bcp1c6603jsn55c3cb22f273" },
-    },
+                    {
+                        { "x-rapidapi-host", "celebrity-bucks.p.rapidapi.com" },
+                        { "x-rapidapi-key", $"{k_ApiKey}" },
+                    }
             };
-            using (var response = await client.SendAsync(request))
+            using(HttpResponseMessage response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 string body = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(body);
                 return XDocument.Parse(body);
             }
-            
+
         }
 
         // Todo Get function to parse document
+        //Todo - Filter the Celebrity name by month number
         public List<string> FilterCelebrityName(XDocument i_XDocument)
         {
             List<string> celebrityNames = new List<string>();
             foreach (XElement element in i_XDocument.Descendants().Where(i_XElement => i_XElement.HasElements == false))
             {
-                string keyName = element.Name.LocalName; //?
-                //string keyDob = element.Name.LocalName; //?
-                /*if (keyDob == "dob")
+                string keyName = element.Name.LocalName; 
+                string keyDob = element.Name.LocalName; 
+                if (keyDob == "dob")
                 {
-                    string[] dateOfBirth = element.Value.Split('-');*/
-                    //int celebBirthMonth = (int) dateOfBirth[1]; //can't convert string to int
+                    string[] dateOfBirth = element.Value.Split('-');
+                    int celebBirthMonth = int.Parse(dateOfBirth[1]); 
                     if(keyName == "name")
                     {
                         celebrityNames.Add(element.Value);
                     }
-               // }
+                }
                 
             }
 
