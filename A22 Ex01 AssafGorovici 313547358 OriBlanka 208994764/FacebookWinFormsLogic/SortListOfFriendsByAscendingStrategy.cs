01cd523105bdf610804b3070c8cdb8848deca473
Assaf Gorovici <assafgorovici@gmail.com>
2022-01-08 21:16:20 +0200

    added Isortstrategy partial implementation

---
+++ A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsLogic/SortListOfFriendsByAscendingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookWinFormsLogic
+{
+    class SortListOfFriendsByAscendingStrategy : ISortListOfFriendsStrategy
+    {
+        public void Sort(List<string> friendsList)
+        {
+            friendsList.Sort((a, b) => a.CompareTo(b));
+        }
+    }
+}
---
+++ A22 Ex01 AssafGorovici 313547358 OriBlanka 208994764/FacebookWinFormsLogic/SortListOfFriendsByDescendingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookWinFormsLogic
+{
+    class SortListOfFriendsByDescendingStrategy : ISortListOfFriendsStrategy
+    {
+        public void Sort(List<string> friendsList)
+        {
+            friendsList.Sort((a, b) => b.CompareTo(a));
+        }
+    }
+}
